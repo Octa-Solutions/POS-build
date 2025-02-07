2ff3cd336f5acb46cb3dbf1bf22630e6f09471df
ahmadothman98 <aothman156@gmail.com>
2025-02-07 18:40:39 +0200

    feat(service): notification service

---
+++ src/app/services/notification/notification.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  constructor(private toastCtrl: ToastController) {
+  }
+  async presentToast(message: string, duration: number, position: any, color: string) {
+    this.toastCtrl.dismiss();
+    const toast = await this.toastCtrl.create({ message, duration, position, color });
+    toast.present();
+  }
+  success(message: string) {
+    this.presentToast(message, 5000, 'bottom', 'success');
+  }
+  error(message: string) {
+    this.presentToast(message, 5000, 'bottom', 'danger')
+  }
+}
