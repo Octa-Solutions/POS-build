4727d4c81918e0157fa694a69be41b3911a31153
ahmadothman98 <aothman156@gmail.com>
2025-02-12 12:46:57 +0200

    feat: boxes component

---
+++ src/app/components/boxes/boxes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxes',
+  templateUrl: './boxes.component.html',
+  styleUrls: ['./boxes.component.scss'],
+  imports: []
+})
+
+export class BoxesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
